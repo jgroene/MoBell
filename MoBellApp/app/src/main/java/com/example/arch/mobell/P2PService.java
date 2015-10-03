152884a4d36d67dfde195f188a9b952ca54ad470
Christophe Piveteau <chripiv@gmail.com>
2015-10-03 06:41:47 +0200

    updated p2pservice

--- MoBellApp/app/src/main/java/com/example/arch/mobell/P2PService.java
+++ MoBellApp/app/src/main/java/com/example/arch/mobell/P2PService.java
@@ -5,12 +5,31 @@ import android.content.Intent;
 import android.os.IBinder;
 
 public class P2PService extends Service {
+
+    public static enum Broadcasts {
+        // broadcast before the session starts
+        onStatusChanged,
+        onDeviceConneted,
+        onDeviceDisconnected,
+        onStartSession,
+        onAbort,
+        // broadcasts after the session starts
+        onDeviceOutOfRange,
+        getDetails
+    };
+
     public P2PService() {
     }
 
+
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String command = intent.getStringExtra("");
+        return Service.START_NOT_STICKY;
     }
+
+    // example of sending a broadcast:
+    //Intent i = new Intent("p2pservice");
+    //i.putExtra("message", Broadcasts.onStatusChanged);
+    //sendBroadcast(i);
 }
