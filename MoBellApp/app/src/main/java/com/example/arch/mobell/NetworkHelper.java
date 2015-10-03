8a57f22407dc39ee5cb6dba6d7431c7c04818bbd
Jannik Gröne <jgroene@student.ethz.ch>
2015-10-03 17:14:30 +0200

    Ip exchange protocol

--- MoBellApp/app/src/main/java/com/example/arch/mobell/NetworkHelper.java
+++ MoBellApp/app/src/main/java/com/example/arch/mobell/NetworkHelper.java
@@ -7,25 +7,36 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 /**
  * Created by jannik on 10/3/15.
  */
 public class NetworkHelper {
-    static String[] addresses;
-    static String[] getAddresses() {
+    public static int size;
+    static List<InetAddress> addresses;
+    static List<InetAddress> getAddresses() {
         return addresses;
     }
+    static void addAddress(InetAddress addr) {
+        addresses.add(addr);
+    }
+    static void setAddresses(List<InetAddress> addrs) {
+        addresses = addrs;
+    }
+    static int distributed = 0;
+    static void increaseDistributed() {distributed++;}
+    static void setSize(int s) { size = s; };
     public static void broadcastIp(InetAddress hostAddress, boolean host) {
         if (host) {
             try {
                 ServerSocket serverSocket = new ServerSocket(8877);
-                Socket client = serverSocket.accept();
-                ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
-                Object object = ois.readObject();
-                if (object.getClass() == String.class && (String) object == "MoBellHandshake") {
-                    
+                while (addresses.size() != size) {
+                    Socket client = serverSocket.accept();
+                    Runnable handler = new NetworkHelperHandler(client);
                 }
+                while (distributed != size) {}
+                serverSocket.close();
             } catch (Exception ex) {
                 //TODO
             }
@@ -37,8 +48,14 @@ public class NetworkHelper {
                 OutputStream os = socket.getOutputStream();
                 ObjectOutputStream oos = new ObjectOutputStream(os);
                 oos.writeChars("MoBellHandshake");
+                socket.setKeepAlive(true);
+                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                Object object = ois.readObject();
+                if(object.getClass() == List.class) {
+                    addresses = (List<InetAddress>) object;
+                }
+                ois.close();
                 oos.close();
-                os.close();
                 socket.close();
             } catch (Exception ex) {
                 //TODO
---
+++ MoBellApp/app/src/main/java/com/example/arch/mobell/NetworkHelperHandler.java
@@ -0,0 +1,34 @@
+package com.example.arch.mobell;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by jannik on 10/3/15.
+ */
+public class NetworkHelperHandler implements Runnable {
+    Socket client;
+    NetworkHelperHandler(Socket c) {
+        client = c;
+    }
+    public final void run() {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
+            Object object = ois.readObject();
+            if (object.getClass() == String.class && (String) object == "MoBellHandshake") {
+                NetworkHelper.addAddress(client.getInetAddress());
+            }
+            while (NetworkHelper.getAddresses().size() != NetworkHelper.size) {}
+            ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
+            oos.writeObject(NetworkHelper.getAddresses());
+            oos.close();
+            ois.close();
+            client.close();
+            } catch (Exception ex) {
+
+            }
+
+    }
+}
